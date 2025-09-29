c8bafded45b51b7c8c708ad0f83403d2b0f2bf77
Baraka1995 <baraksteven93@gmail.com>
2025-09-29 11:28:57 +0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+import React from "react";
+import Socials from "@/Components/socials";
+export default function Home() {
+  return (
+    <>
+      <Socials />
+    </>
+  );
+}
